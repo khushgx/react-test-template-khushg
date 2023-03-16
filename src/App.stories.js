178a5f9c0215e736aa846e0d938acb67256cd407
khushgx <78624519+khushgx@users.noreply.github.com>
2023-03-15 20:39:04 -0400

    Update App.stories.js

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,17 @@
-// Stories for testing `App` go here
-// See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import App from './App';
+
+export default {
+  title: 'App',
+  component: App,
+};
+
+export const Default = () => (
+  <App />
+);
+
+export const ButtonClick = () => (
+  <App onClick={action('button-click')} />
+);
