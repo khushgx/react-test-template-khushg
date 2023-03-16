1b44302919ceec1cce4ae097cb4a6729aa2a0288
khushgx <78624519+khushgx@users.noreply.github.com>
2023-03-15 20:36:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import React from 'react';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
-      
+      isToggleOn: true
     };
+    
+    this.handleClick = this.handleClick.bind(this);
   }
 
   //handler goes here
+  
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
 
   render() {
     return (
       <div>
         <p>Greetings!</p>
         <p>Button goes here</p>
+        <button onClick={this.handleClick}> {this.state.isToggleOn ? 'Click Me' : 'Thanks'} </button>
       </div>
     );
   }
